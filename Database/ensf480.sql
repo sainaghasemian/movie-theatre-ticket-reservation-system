618e8a5dd613923b7fbd3612c6bc25a4870c95ca
Racheld3 <47041999+Racheld3@users.noreply.github.com>
2022-12-02 22:29:53 -0700

    update sql file to work with xampp

--- Database/ensf480.sql
+++ Database/ensf480.sql
@@ -1,25 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 02, 2022 at 09:46 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Host: 127.0.0.1    Database: ensf480
+-- ------------------------------------------------------
+-- Server version	8.0.28
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `ensf480`
---
+DROP DATABASE IF EXISTS `ENSF480`;
+CREATE DATABASE `ENSF480` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `ENSF480`;
 
 -- --------------------------------------------------------
 
@@ -216,6 +204,3 @@ ALTER TABLE `showtime`
   ADD CONSTRAINT `theatreID` FOREIGN KEY (`theatreID`) REFERENCES `theatre` (`theatreID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
